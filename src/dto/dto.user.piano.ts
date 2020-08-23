1bc0c9934755f9767b3dacf01ed83bba8eb8db8b
Byungchan Lim <bl458@cornell.edu>
2020-08-23 14:09:17 +0900

    feat: Add validation to piano user dto

--- src/dto/dto.user.piano.ts
+++ src/dto/dto.user.piano.ts
@@ -1,11 +1,30 @@
-import { isEmail, isString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  MaxLength,
+  Length,
+} from 'class-validator';
 
 export class CreatePianoUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(254)
   readonly email: string;
 
+  @IsNotEmpty()
+  @Length(8, 25)
   readonly pw: string;
 
+  @IsNotEmpty()
+  @MaxLength(35)
   readonly firstName: string;
 
+  @IsOptional()
+  @MaxLength(35)
+  readonly middleName: string;
+
+  @IsNotEmpty()
+  @MaxLength(35)
   readonly lastName: string;
 }
