ea585262ea9452c285b2eda9b4877cc93f8427c4
Byungchan Lim <bl458@cornell.edu>
2020-08-23 17:51:12 +0900

    feat: Add new piano user session service, controller to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserPianoService } from './piano/user.piano.service';
 import { UserPianoController } from './piano/user.piano.controller';
 import { AuthModule } from '../auth/auth.module';
+import { UserPianoSessionService } from './piano/user.piano.session.service';
+import { UserPianoSessionController } from './piano/user.piano.session.controller';
 
 @Module({
   imports: [AuthModule],
-  providers: [UserPianoService],
-  controllers: [UserPianoController],
+  providers: [UserPianoService, UserPianoSessionService],
+  controllers: [UserPianoController, UserPianoSessionController],
 })
 export class UserModule {}
