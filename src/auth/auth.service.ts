3c06b248bd2737a695f62abe7556a442edc2efff
Byungchan Lim <bl458@cornell.edu>
2020-08-23 12:49:25 +0900

    chore: Install bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { randomBytes } from 'crypto';
+import {hash} from 'bcrypt'
 
 const HASH_ROUNDS = 10;
 const TOKEN_BYTES = 256;
@@ -8,16 +9,21 @@ const TOKEN_BYTES = 256;
 export class AuthService {
   //Generate new token
   generate(): Promise<string> {
-    return;
+    return new Promise((resolve, reject) =>
+      randomBytes(TOKEN_BYTES, (err, buff) => {
+        if (err) reject(err)
+        return resolve(buff.toString('hex'));
+      }),
+    );
   }
 
   //Hash rawPw
-  hashPw(rawPw: string): Promise<string> {
-    return;
+  hashPw(rawPw: string): Promise<String> {
+    return new Promise((resolve,reject) => )
   }
 
   //Compare inputted pw and hashedPw in db for authentication
-  compare(inputPw: string, hashedPw: string): Promise<boolean> {
+  compare(inputPw: string, hashedPw: string): Promise<Boolean> {
     return;
   }
 }
