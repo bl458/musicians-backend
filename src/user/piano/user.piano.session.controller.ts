5f9e567300ba3456f7d519560f7e3805e014c9cd
Byungchan Lim <bl458@cornell.edu>
2020-08-23 18:01:09 +0900

    feat: Set up placeholder functions for piano user session service, controller

--- src/user/piano/user.piano.session.controller.ts
+++ src/user/piano/user.piano.session.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Delete, Param } from '@nestjs/common';
+import { UserPianoSessionService } from './user.piano.session.service';
 
 @Controller()
-export class UserPianoSessionController {}
+export class UserPianoSessionController {
+  constructor(private pSService: UserPianoSessionService) {}
+
+  @Post('/puser/sessions')
+  async login(): Promise<string> {
+    return;
+  }
+
+  @Delete('/puser/sessions/:token')
+  async logout(@Param('token') token: string): Promise<void> {
+    return;
+  }
+}
--- src/user/piano/user.piano.session.service.ts
+++ src/user/piano/user.piano.session.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DBConnService } from 'src/db/db.conn.service';
 
 @Injectable()
-export class UserPianoSessionService {}
+export class UserPianoSessionService {
+  constructor(private conn: DBConnService) {}
+
+  async authenticate(): Promise<string> {
+    return;
+  }
+
+  async deauthenticate(token: string): Promise<void> {
+    return;
+  }
+}
