ed6d5b05b1533e019114409844478f1fca50e9f2
Byungchan Lim <bl458@cornell.edu>
2020-08-23 13:12:37 +0900

    feat: Create service to create new piano user

--- src/user/piano/user.piano.service.ts
+++ src/user/piano/user.piano.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
-export class UserPianoService {}
+export class UserPianoService {
+  constructor(private authService: AuthService) {}
+
+  //Create new piano user, stores to db
+  async createNew(email: string, pw: string) {
+    let newToken = await this.authService.generate();
+  }
+}
