423650c781f06167080dacd1a02342741560f7bd
Byungchan Lim <bl458@cornell.edu>
2020-08-25 21:15:49 +0900

    feat: Create piano practice controller, service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,13 +4,23 @@ import { AuthModule } from '../auth/auth.module';
 
 import { UserPianoController } from './piano/user.piano.controller';
 import { UserPianoSessionController } from './piano/user.piano.session.controller';
+import { UserPianoPracticeController } from './piano/user.piano.practice.controller';
 
 import { UserPianoService } from './piano/user.piano.service';
 import { UserPianoSessionService } from './piano/user.piano.session.service';
+import { UserPianoPracticeService } from './piano/user.piano.practice.service';
 
 @Module({
   imports: [AuthModule],
-  providers: [UserPianoService, UserPianoSessionService],
-  controllers: [UserPianoController, UserPianoSessionController],
+  providers: [
+    UserPianoService,
+    UserPianoSessionService,
+    UserPianoPracticeService,
+  ],
+  controllers: [
+    UserPianoController,
+    UserPianoSessionController,
+    UserPianoPracticeController,
+  ],
 })
 export class UserModule {}
